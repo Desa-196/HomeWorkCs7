d20e2fcde33ac35c4da96a7777d5ff093671b140
Денис Пономарев <deskin196@gmail.com>
2023-03-28 21:01:32 +0300

    Написал алгоритм по дополнительному домашнему заданию 3

--- DOP3/Program.cs
+++ DOP3/Program.cs
@@ -12,3 +12,75 @@
 9   13  17  21  25  29  33  36  38  39
 
 */
+
+int[] ReadPositionArrayElement()
+{
+    Console.Write("Введите 2 числа позиции элемента в двумерном массиве через пробел: ");
+
+    int[] IntReadString = new int[2];
+    while (true)
+    {
+
+        string[] ReadString = Console.ReadLine()!.Split();
+        if (ReadString.Length != 2)
+        {
+            Console.Write("Необходимо ввести 2 числа позиции, повторите ввод: ");
+        }
+        else if (int.TryParse(ReadString[0], out IntReadString[0]) && int.TryParse(ReadString[1], out IntReadString[1]))
+        {
+            break;
+        }
+        else
+        {
+            Console.Write("Введены некорректные числа, повторите попытку ввода:");
+        }
+    }
+    return IntReadString;
+}
+
+void FillRandomArray(int[,] array)
+{
+    int count = 0;
+    //Проходимся по всем столбцам и начиная с верхней строки двигаемся по диаганали
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0, k = j; i < array.GetLength(0) && k >= 0; i++, k--)
+        {
+            array[i, k] = count;
+            count++;
+        }
+    }
+    // Как только прошлись по всем столбцам, дозаполняем диагонали начинающиеся с концов строк, кроме нулевой строки.
+    for(int i=1; i < array.GetLength(0); i++)
+    {
+        for(int k=i, j=array.GetLength(1) - 1; k < array.GetLength(0) && j >= 0; k++, j--)
+        {
+            array[k, j] = count;
+            count++;
+        }
+    }
+}
+
+//Выводит массив в консоль
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+//Считываем из консоли размерность массива
+int[] PositionArray = ReadPositionArrayElement();
+//Создаем массив
+int[,] array = new int[PositionArray[0], PositionArray[1]];
+//Заполняем случайными числами
+FillRandomArray(array);
+//выводим получившийся массив
+PrintArray(array);
