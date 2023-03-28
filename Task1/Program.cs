7fd463cc8a8433fb4ce1b76e46052053dc362af3
Денис Пономарев <deskin196@gmail.com>
2023-03-28 14:38:37 +0300

    Написал алгоритм по домашнему заданию 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -7,3 +7,56 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 
 */
+
+
+int read_int_from_console(string message)
+{
+    int console_int = 0;
+
+    Console.Write($"{message}: ");
+
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out console_int))
+        {
+            break;
+        }
+        else
+        {
+            Console.Write("Введено некорректное число, повторите попытку ввода:");
+        }
+    }
+    return console_int;
+
+}
+//Заполняем массив случайными вещественными числами от min до max
+void FillRandomArray(double[,] array, int min, int max)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Math.Round( new Random().NextDouble()*(max - min) + min, 2 );
+        }
+    }
+}
+//Выводит массив в консоль
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+double[,] array = new double[read_int_from_console("Введите размер массива m"), read_int_from_console("Введите размер массива n")];
+
+Console.WriteLine();
+FillRandomArray(array, -100, 100);
+PrintArray(array);
